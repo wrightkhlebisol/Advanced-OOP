7336f3fb58da6666fe93402452f8d974c8e72a81
Wright <wrightkhlebisol@gmail.com>
2020-03-26 13:09:55 +0100

    Getters and Setters

--- CLASSES/Task.php
+++ CLASSES/Task.php
@@ -1,15 +1,29 @@
-<?php 
-    # Why Tasks: Because OOP says, hunt down the nouns
+<?php
+# Why Tasks: Because OOP says, hunt down the nouns
 
-class Task{
+class Task
+{
     public $description;
 
-    public function __construct($task){
-        var_dump('CALLED');
-        $this->description = $task;
-        echo $this->description;
+    public $title;
+
+    public $completed = false;
+
+    public function __construct($task_title, $task_desc)
+    {
+        $this->title = $task_title;
+        $this->description = $task_desc;
+        var_dump($this->title, $this->description);
+    }
+
+    public function complete()
+    {
+        $this->completed = true;
     }
 }
 
-$task = new Task('Learn OOP');
-// var_dump($task->description);
+$task1 = new Task('Learn OOP', 'Here is some description for task1');
+$task2 = new Task('Pick up groceries', 'And another description specifically for task2');
+
+$task2->complete();
+// var_dump($task1->completed);
