e1dfe3b41af2861c08b29ddadcb043f99bc2e4c8
Wright <wrightkhlebisol@gmail.com>
2020-04-02 21:44:10 +0100

    Statics as constants

--- STATCONST/Math.php
+++ STATCONST/Math.php
@@ -1,10 +1,24 @@
 <?php
 class Math
 {
-    public function add(...$args)
+    public static function add(...$args)
     {
         return array_sum($args);
     }
 }
 
-echo (new Math)->add(1, 2, 3, 4, 5);
+class NewPerson
+{
+    public static $age = 0;
+
+    public function haveBirthday()
+    {
+        return static::$age += 1;
+    }
+}
+
+// echo (new Math)->add(1, 2, 3, 4, 5);
+
+$joe = (new NewPerson)->age;
+echo $joe;
+// echo $joe->haveBirthday();
